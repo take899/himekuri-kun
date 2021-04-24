8c4c4ecde1f9f6295608d4ccb30631204d885a93
Takeshi Notsu <notsutakeshi@gmail.com>
2021-04-25 01:17:48 +0900

    added wall paper

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+// import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      hello world
-    </div>
-  );
+    <React.Fragment>
+      <div className="absolute w-screen h-screen __bg-image"></div>
+      <div className="relative w-64 mx-auto bg-white shadow-2xl top-14 h-96">
+        calendar
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default App;
